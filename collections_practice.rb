e84b9da21fd590fdb83ab936ba189b0ef3c158ee
Erin  Rose <errose10@gmail.com>
2019-11-22 14:14:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+require 'pry'
 
+def sort_array_asc(stuff)
+  stuff.sort
+end
+
+def sort_array_desc(stuff)
+  stuff.sort.reverse
+end
+
+def sort_array_char_count(stuff)
+  stuff.sort_by { |obj| obj.length }
+end
+
+def swap_elements(stuff)
+
+end
+
+def reverse_array(stuff)
+
+end
+
+def kesha_maker(stuff)
+
+end
+
+def find_a(stuff)
+
+end
+
+def sum_array(stuff)
+
+end
+
+def add_s
+
+end
